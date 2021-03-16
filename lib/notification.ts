f7d9faf3cac7b86c1fb5a05961d6df9da9c10808
Kaleb Mills <keam.mills@gmail.com>
2021-03-16 05:02:17 +0000

    Added CommandClient dependency

--- lib/notification.ts
+++ lib/notification.ts
@@ -1,7 +1,10 @@
 import { IInitializable, ICloseable, Logger, LogLevel } from './base';
 import * as discord from 'discord.js';
 import joi from 'joi';
-import { InvalidConfigurationError } from './exceptions';
+import { DefaultError, InvalidConfigurationError } from './exceptions';
+import { EventEmitter } from 'events';
+import { inspect } from 'util';
+import color from 'randomcolor';
 
 export interface NotificationOptions {
     ticker: string;
@@ -27,12 +30,176 @@ export interface DiscordChannels {
     notificationChannel: string;
 }
 
+/*
+    Wouldn't be a bad idea to move this to a separate file, but since notifications and commands are so intertwined, it's fine for now.
+*/
+export type CommandHandler = () => Promise<string>;
+
+export interface CommandContainer {
+    handler: CommandHandler;
+    description: string;
+    registrar: string; //Who registered the command
+    command: string;
+}
+
+export interface CommandClient extends IInitializable, ICloseable {
+    registerCommandHandler(options: CommandContainer): void;
+}
+
+export class PhonyCommandClient implements CommandClient {
+    constructor() {
+
+    }
+
+    initialize(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    registerCommandHandler(handler: CommandContainer): void {
+        return;
+    }
+
+    close(): Promise<void> {
+        return Promise.resolve();
+    }
+    
+}
+
+/*
+    TODO: As a matter of good design, the DiscordClient should be a separate entity from another abstract class
+    we should turn this into, like DiscordCommandClient, which has the DiscordClient as a dependency, to honor the SRP principle.
+    For simplicity though, this is fine for now.
+*/
+export class DiscordClient extends EventEmitter implements CommandClient {
+    private readonly token: string;
+    private logger: Logger;
+    private commandHandlers: { [command: string]: CommandContainer };
+    private _client!: discord.Client;
+    private readonly commandPrefix: string;
+
+    constructor(options: DiscordClientOptions) {
+        super();
+        this.token = options.token;
+        this.logger = options.logger;
+        this.commandHandlers = {};
+        this.commandPrefix = options.commandPrefix;
+    
+        this.logger.log(LogLevel.INFO, `${this.constructor.name}#constructor():INVOKED`);
+        this.registerCommandHandler({
+            command: 'help',
+            description: 'Help command to list all available commands.',
+            handler: () => {
+                    let cmds = Object.keys(this.commandHandlers).map(key => {
+                        const { command, description, registrar } = this.commandHandlers[key];
+
+                        return `**${command}**: ${description}\nRegistered By: **${registrar}**`
+                    })
+                    cmds.unshift('\n');
+
+                    return Promise.resolve(cmds.join('\n\n'));
+                
+            },
+            registrar: this.constructor.name
+        })
+    }
+
+    initialize(): Promise<void> {
+        this._client = new discord.Client();
+
+        return this._client.login(this.token)
+        .then(() => {
+            this._client.on('message', this._handleIncomingMessage);
+
+            this.logger.log(LogLevel.INFO, `${this.constructor.name}#initialize():SUCCESS`);
+        });
+    }
+
+    registerCommandHandler(options: CommandContainer): void {
+        const { command, handler, description, registrar } = options;
+        if (!this.commandHandlers.hasOwnProperty(command)) {
+            let data = {
+                commandRegisteredBy: registrar,
+                command,
+                commandDescription: description
+            }
+            this.logger.log(LogLevel.INFO, inspect(data));
+
+            this.commandHandlers[command] = {
+                handler,
+                command,
+                registrar,
+                description
+            }
+
+            if (description) {
+                this.commandHandlers[command].description = description;
+            }
+        } else {
+            throw new DefaultError(`${command} already has a handler registered for it.`);
+        }
+    }
+
+    private _handleIncomingMessage = (message: discord.Message): void => {
+        console.log(`Incoming Message: ${message.content}`)
+        let isCommand = message.content.trim().startsWith(this.commandPrefix);
+
+        console.log(`is command: ${isCommand} -- prefix = ${this.commandPrefix}`);
+
+        if (isCommand) {
+            let command = message.content.substring(1);
+
+            console.log(JSON.stringify(Object.keys(this.commandHandlers)));
+
+            if (this.commandHandlers.hasOwnProperty(command)) {
+                console.log(`Command has handler == true`)
+                let handler = this.commandHandlers[command].handler;
+                handler()
+                .then(data => {
+                    let embed = new discord.MessageEmbed();
+
+                    embed.setDescription(data);
+                    embed.setColor(color());
+
+                    return message.reply(embed);
+                })
+                .catch(this.errorHandler)
+            } else {
+                message.reply(`${command} is not a command, please see !help for assistance`)
+                .catch(this.errorHandler);
+            }
+        } else {
+            return;
+        }
+    }
+
+    getClient(): discord.Client {
+        return this._client;
+    }
+
+    errorHandler(err: any) {
+        this.logger.log(LogLevel.ERROR, `${this.constructor.name}#errorHandler -- ${err}`);
+    }
+
+    close(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            try {
+                this._client.destroy()
+                return resolve();
+            } catch (e) {
+                return reject(e);
+            }
+        });
+    }
+
+}
+
+
 export interface DiscordNotificationOptions extends BaseNotificationOptions {
     guildId: string;
     logger: Logger;
     channels: DiscordChannels;
     token: string;
-    client: discord.Client;
+    client: DiscordClient;
 }
 
 const DiscordOptionsSchema: joi.Schema = joi.object({
@@ -42,16 +209,26 @@ const DiscordOptionsSchema: joi.Schema = joi.object({
         notificationChannel: joi.string().required()
     }).required(),
     token: joi.string().required(),
-    client: joi.object({}).required(),
+    client: joi.object().instance(DiscordClient).required(),
     logger: joi.object({}).required()
 }).required();
+
+/**
+ * Wrapper for a Discord Client, which specifically passes messages to a event handler that are commands. Helper funtions for registering commands from other clients
+ */
+
+interface DiscordClientOptions {
+    token: string;
+    logger: Logger;
+    commandPrefix: string;
+}
 
 /*
     Currently this implementation expects to only work in a single Guild, and does not
     account for use in multiple guilds.
 */
 export class DiscordNotification implements INotification {
-    private client: discord.Client;
+    private client: DiscordClient;
     private readonly token: string;
     private logger: Logger;
     private guildId: string;
@@ -73,14 +250,11 @@ export class DiscordNotification implements INotification {
     }
 
     initialize(): Promise<void> {
-        return this.client.login(this.token)
-        .then(() => {
-            this.logger.log(LogLevel.INFO, `${this.constructor.name}#initialize():SUCCESS`);
-        });
+        return Promise.resolve();
     }
 
     notify(message: NotificationOptions): Promise<void> {
-        return this.client.guilds.fetch(this.guildId, undefined, true)
+        return this.client.getClient().guilds.fetch(this.guildId, undefined, true)
         .then(guild => guild.channels)
         .then(channels => channels.cache.find(c => c.name === this._selectChannel(message))!)
         .then(channel => channel as discord.TextChannel)
@@ -107,7 +281,7 @@ export class DiscordNotification implements INotification {
 
                 if (message.additionaData) {
                     Object.keys(message.additionaData).forEach(key => {
-                        embed.addField(key, message.additionaData![key]);
+                        embed.addField(key, message.additionaData![key], true);
                     });
                 }
 
@@ -126,10 +300,7 @@ export class DiscordNotification implements INotification {
     }
 
     close(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            this.client.destroy();
-            resolve();
-        });
+        return Promise.resolve();
     }
 }
 
